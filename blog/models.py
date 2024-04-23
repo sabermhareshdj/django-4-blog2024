5671f4e64baf017ba2b56f79e9c1fc4420aab289
sabermhareshdj <dj.saber.mharesh.com>
2024-04-23 21:26:25 +0300

    add simple blog model

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField(max_length=30000)
+  create_date = models.DateTimeField(default=timezone.now())
+  draft = models.BooleanField(default=True)
