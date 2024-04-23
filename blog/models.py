a47a5903f8ee27eda55ab0699611736f73ad741c
sabermhareshdj <dj.saber.mharesh.com>
2024-04-23 22:31:27 +0300

    add post image

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,5 @@ class Post(models.Model):
   draft = models.BooleanField(default=True)
   tags = TaggableManager()
   author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='post_user')
+  image = models.ImageField(upload_to='posts')
 
