43e0a4f728092e940f488d9adad8bc4de0ddb2cd
sabermhareshdj <dj.saber.mharesh.com>
2024-04-24 12:06:17 +0300

    add create new post

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
   data = Post.objects.all()
-
   return render(request,'post_list.html',{'posts':data})
 
-
-
-
-
 def post_detail(request,post_id):
-
   data = Post.objects.get(id=post_id)
-
   return render(request,'post_detail.html',{'post':data})
+
+
+def post_new(request):
+  if request.method == 'POST':
+    form =PostForm(request.POST,request.FILES)
+    if form.is_valid():
+      form.save()
+  else:
+    form = PostForm
+  return render(request,'new_post.html',{'form':form})
+
+
+
+def edit_post(repuest,post_id):
+  pass
+
+
+def delete_post(request,post_id):
+  pass
